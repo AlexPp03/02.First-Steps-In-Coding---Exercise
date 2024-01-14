fd80b012d38b7cd7b6db9750d0f7d72b9d588867
AlexPp03 <156581032+AlexPp03@users.noreply.github.com>
2024-01-14 13:56:39 +0200

    Add files via upload

---
+++ 08. Basketball Equipment/Program.cs
@@ -0,0 +1,10 @@
+﻿int tax = int.Parse(Console.ReadLine());
+
+double shoes = tax - tax * 0.4;
+double outfit = shoes - shoes * 0.2;
+double ball = outfit * 0.25;
+double acc = ball / 5;
+
+double finalPrice =  tax + shoes + outfit + ball + acc;
+
+Console.WriteLine(finalPrice);
