199ab96b957f02458ef69e93256a27cea8329b3a
AlexPp03 <156581032+AlexPp03@users.noreply.github.com>
2024-01-14 13:57:15 +0200

    Add files via upload

---
+++ 09. Fish Tank/Program.cs
@@ -0,0 +1,12 @@
+﻿int lenght = int.Parse(Console.ReadLine());
+int width = int.Parse(Console.ReadLine());
+int height = int.Parse(Console.ReadLine());
+double procent = double.Parse(Console.ReadLine());
+
+int size = lenght * width * height;
+double sizeL = size * 0.001;
+double procentR = procent / 100.0;
+
+double liters = sizeL * (1 - procentR);
+
+Console.WriteLine(liters);
