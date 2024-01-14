539334b2da4e6d52d0714c12ace6d8022b9dfca0
AlexPp03 <156581032+AlexPp03@users.noreply.github.com>
2024-01-14 13:56:06 +0200

    Add files via upload

---
+++ 07. Food Delivery/Program.cs
@@ -0,0 +1,16 @@
+﻿int chiken = int.Parse(Console.ReadLine());
+int fish = int.Parse(Console.ReadLine());
+int vegetarian = int.Parse(Console.ReadLine());
+
+double chikenP = chiken * 10.35;
+double fishP = fish * 12.40;
+double vegetarianP = vegetarian * 8.15;
+double sum = chikenP + fishP + vegetarianP;
+
+double desert = sum * 0.20;
+
+double finalPrice = sum + desert + 2.50;
+
+Console.WriteLine(finalPrice);
+
+
