e8ae83fc0435c15ecf61b4d5685fdba3d010acba
AlexPp03 <156581032+AlexPp03@users.noreply.github.com>
2024-01-14 13:54:18 +0200

    Add files via upload

---
+++ 04. Vacation Books List/Program.cs
@@ -0,0 +1,8 @@
+﻿int pagesC = int.Parse(Console.ReadLine());
+int pages = int.Parse(Console.ReadLine());
+int days = int.Parse(Console.ReadLine());
+
+int totalTime = pagesC / pages;
+int hoursPerDay = totalTime / days;
+
+Console.WriteLine(hoursPerDay);
