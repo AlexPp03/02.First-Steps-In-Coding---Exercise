3f91a4bc7836a25d2135764faddd8f0099230ed5
AlexPp03 <156581032+AlexPp03@users.noreply.github.com>
2024-01-14 13:53:46 +0200

    Add files via upload

---
+++ 03. Deposit Calculator/Program.cs
@@ -0,0 +1,7 @@
+﻿double deposit = double.Parse(Console.ReadLine());
+int term = int.Parse(Console.ReadLine());
+double rate = double.Parse(Console.ReadLine()) / 100;
+
+double sum = deposit + term * (deposit * rate / 12);
+
+Console.WriteLine(sum);
