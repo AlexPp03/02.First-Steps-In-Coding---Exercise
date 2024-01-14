b54a112cc179c1097f6792e6b2c45c5ec2953180
AlexPp03 <156581032+AlexPp03@users.noreply.github.com>
2024-01-14 13:54:54 +0200

    Add files via upload

---
+++ 05. Supplies for School/Program.cs
@@ -0,0 +1,13 @@
+﻿int pens = int.Parse(Console.ReadLine());
+int markers = int.Parse(Console.ReadLine());
+int preparations = int.Parse(Console.ReadLine());
+double discount = double.Parse(Console.ReadLine()) / 100;
+
+double sum = (pens * 5.80) + (markers * 7.20) + (preparations * 1.20);
+double finalPrice = sum - (sum * discount);
+Console.WriteLine(finalPrice);
+   
+
+
+
+
